a4e74c81def3e1a06d7d954cb0f2298cf3c8f315
Danii <daniellaadegun@gmail.com>
2026-09-10 10:05:37 -0600

    These codes are for making shapes and letters. The first file follows the readme and I made a square and wrote out the word "hello". The second file is for my personal work. being able to exercise my knowledge from the readme. I drew house and wrote out my name using the turtle cursor

---
+++ root/main.py
@@ -0,0 +1,77 @@
+from turtle import *
+color("pink")
+bgcolor("blue")
+forward(100)
+color("red")
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+reset()
+
+letter_size = 30
+gap = 10
+penup()
+backward_length = (letter_size * 2) + (gap * 2)
+backward(backward_length)
+# H
+pendown()
+left(90)
+forward(letter_size * 2)
+backward(letter_size)
+right(90)
+forward(letter_size)
+left(90)
+forward(letter_size)
+backward(letter_size * 2)
+right(90)
+penup()
+forward(gap)
+
+# e
+pendown()
+left(90)
+forward(letter_size)
+right(90)
+forward(letter_size)
+right(90)
+forward(letter_size / 2)
+right(90)
+forward(letter_size)
+left(90)
+forward(letter_size / 2)
+left(90)
+forward(letter_size)
+penup()
+forward(gap)
+
+# l
+pendown()
+left(90)
+forward(letter_size * 2)
+right(90)
+penup()
+forward(gap)
+
+# l
+pendown()
+right(90)
+forward(letter_size * 2)
+left(90)
+penup()
+forward(gap)
+
+# o
+pendown()
+forward(letter_size)
+left(90)
+forward(letter_size)
+left(90)
+forward(letter_size)
+left(90)
+forward(letter_size)
+
+done()
+
